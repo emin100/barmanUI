c3565386e3a802e1f6f0ae0f7288cb467c6ba7a0
mehmet emin karakas <mkarakas@turksat.com.tr>
2016-05-06 17:01:47 +0300

    Layout

--- barmanui/app.py
+++ barmanui/app.py
@@ -1,34 +1,29 @@
-from flask import Flask, g, make_response, request
+from flask import Flask, render_template, g, make_response, request, jsonify
 from flask.ext.httpauth import HTTPTokenAuth, HTTPBasicAuth
 from flask.ext.restful import Api
 from flask.ext.script import Manager
-
-
-
 
 app = Flask(__name__, instance_relative_config=True)
 api = Api(app)
 manager = Manager(app)
 
 
-def write_log(error_message, error_type):
-    if hasattr(g, 'user'):
-        message = ('User: %s ' % g.user.get('username'))
-    else:
-        message = ('User: %s ' % None)
-    message += (str(error_type) + ': %s ' % (error_message))
-    if request.url:
-        message += ('Url: %s ' % request.url)
-    if request.args:
-        message += ('Arg: %s ' % request.args)
-    app.logger.error(message)
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/echo/', methods=['GET'])
+def echo():
+    ret_data = {"value": request.args.get('echoValue')}
+    return jsonify(ret_data)
 
 
 @manager.command
 def runserver():
     """Start BARMAN API Server"""
 
-    app.run()
+    app.run(debug=True)
 
 
 def main():
