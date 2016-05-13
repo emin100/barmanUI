538eef8ec93f6f151d35581744c0731f0e43b3ff
mehmet emin karakas <mkarakas@turksat.com.tr>
2016-05-13 17:57:17 +0300

    Auth and Login

--- barmanui/app.py
+++ barmanui/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+
+
+from flask import Flask, render_template,request
 from flask.ext.login import LoginManager, login_required
 from flask.ext.restful import Api
 from flask.ext.script import Manager
+
 from auth import User
 from parser import ConfigParser
 
@@ -15,7 +18,7 @@ config_main = ConfigParser('/etc/barmanui.conf')
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get()
+    return User.get(user_id)
 
 
 @login_manager.unauthorized_handler
@@ -38,6 +41,29 @@ def unauthorized():
 @login_required
 def hello():
     return render_template('index.html')
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    password = request.form.get('inputPassword')
+    username = request.form.get('inputUsername')
+    user = User.get(username,password)
+
+    # user = User.query.get(form.email.data)
+    # if user:
+    #     if bcrypt.check_password_hash(user.password, form.password.data):
+    #         user.authenticated = True
+    #         db.session.add(user)
+    #         db.session.commit()
+    #         login_user(user, remember=True)
+    #         return redirect(url_for("bull.reports"))
+
+    return 'dfdssdf'
+
+
+@app.route("/logout")
+def logout():
+    pass
 
 
 @manager.command
